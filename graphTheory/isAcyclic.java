bae30af8441859c288eaa6c00c292a5dcade630a
unknown <patelhet32@gmail.com>
2020-05-31 19:07:10 -0400

    new

--- graphTheory/isAcyclic.java
+++ graphTheory/isAcyclic.java
@@ -51,3 +51,34 @@ public class isAcyclic {
         hm.remove(index);
     }
 }
+
+/*
+
+Given a directed graph, output YES if it is acyclic, and NO otherwise.
+
+Input
+As usual.
+N ≤ 1000.
+
+Output
+YES or NO.
+
+Sample Input
+3
+0 1 0 
+0 0 1
+1 0 0
+
+Sample Output
+NO
+
+Sample Input 2
+3
+0 1 0
+0 0 1
+0 0 0
+
+Sample Output 2
+YES
+
+*/
